c8662e842148775b6170eb3ab74808776a1c00cc
Beycker <beycker_111@hotmail.com>
2019-03-14 22:49:27 -0500

    Maquina de Moore completa, particiones + verificacion equivalencia de maquinas

--- PrototipoMaquinasEquivalentes/PrototipoMaquinasEquivalentes/Form1.cs
+++ PrototipoMaquinasEquivalentes/PrototipoMaquinasEquivalentes/Form1.cs
@@ -331,7 +331,15 @@ namespace PrototipoMaquinasEquivalentes
             listaDeEstados.Add(nuevoEstado);
             for (int i = 0; i < cantidadColumnasMaquinas; i++)
             {
-                matrizM2[filasAgregadasHastaAhora, i] = filaN[i] + _ESP;
+                if(i == cantidadColumnasMaquinas - 1)
+                {
+                    matrizM2[filasAgregadasHastaAhora, i] = filaN[i];
+                }
+                else
+                {
+                    matrizM2[filasAgregadasHastaAhora, i] = filaN[i] + _ESP;
+                }
+                
             }
             filasAgregadasHastaAhora++;
 
@@ -466,8 +474,209 @@ namespace PrototipoMaquinasEquivalentes
                 }
             }
 
-            Console.WriteLine("fin");
+            //Console.WriteLine("fin");
             //Prueba para ver si estoy conectado
+            particionamientoMoore();
+        }
+
+        public void particionamientoMoore()
+        {
+
+            //INICIO PARTICION 1
+
+            string[,] matrizAux = matrizSumaDirecta;
+            List<List<string>> lista = new List<List<string>>(); // Aquí serán guardadas las particiones
+            for (int i = 0; i < (cantidadEstadosM1 + cantidadEstadosM2); i++)
+            {
+                if(matrizAux[i,cantidadColumnasMaquinas - 1] != "")
+                {
+                    string salida = matrizAux[i, cantidadColumnasMaquinas - 1];
+                    matrizAux[i, cantidadColumnasMaquinas - 1] = "";
+                    List<string> listaN = new List<string>();
+                    listaN.Add(matrizAux[i, 0]);
+
+                    for (int j = i; j < (cantidadEstadosM1 + cantidadEstadosM2); j++)
+                    {
+                        if (matrizAux[j, cantidadColumnasMaquinas - 1] != "")
+                        {
+                            if(salida == matrizAux[j, cantidadColumnasMaquinas - 1])
+                            {
+                                matrizAux[j, cantidadColumnasMaquinas - 1] = "";
+                                listaN.Add(matrizAux[j, 0]);
+                            }
+                        }
+                    }
+                    lista.Add(listaN);
+
+                }
+            }
+
+            Console.WriteLine("Particion 1 OK");
+
+            //FIN PARTICION 1
+
+            //COMIENZAN LAS DEMAS PARTICIONES
+            //Se acabara cuando la longitud de lista no haya cambiado
+
+            //notas
+            //En algun punto estrategico debo crear una nueva lista que almacene las nuevas particiones que se generan
+            Boolean cierre = false;
+            while (!cierre)
+            {
+                List<List<string>> auxP1 = lista; //En esta lista se agregaran todos los cambios de la nueva particion
+                int conjuntos = auxP1.Count;
+
+                for (int i = 0; i < conjuntos; i++)
+                {
+                    List<string> listaM = lista.ElementAt(i);
+
+                        int posicionCero = 0;
+                        string estadoA = listaM.ElementAt(posicionCero); //Este sera A
+                        List<string> listaNuevaM = new List<string>(); //Esta será la lista donde vayan los valores que ya no esten en la particon
+                        //Este parece ser un buen lugar para agregar la lista de string
+                        for(int k = posicionCero+1; k < listaM.Count; k++)
+                        {
+                            string estadoB = listaM.ElementAt(k);
+                            //Ya que tenemos los dos estados a comparar, los buscamos en la matriz
+                            int filaA = -1;
+                            int filaB = -1;
+                            //Vamos a buscar la fila de A
+                            for(int l = 0; l < (cantidadEstadosM1 + cantidadEstadosM2); l++)
+                            {
+                                if(estadoA == matrizSumaDirecta[l, 0])
+                                {
+                                    filaA = l;
+                                }else if (estadoB == matrizSumaDirecta[l, 0])
+                                {
+                                    filaB = l;
+                                }
+                                //Cuando ya encontramos las posiciones en las filas de los estados entonces cortamos.
+                                if(filaA != -1 && filaB != -1)
+                                {
+                                    break;
+                                }
+                            }
+
+                            //Ahora tenemos que ver si todos los estimulos de esas dos filas corresponden a un grupo de la lista
+                            //Boolean coso = lista.ElementAt(0).Contains("T");
+                            Boolean seParticiono = false;
+                            for(int h = 1; h < (cantidadColumnasMaquinas - 1) && !seParticiono; h++)
+                            {
+                                //Obtenemos aqui cada uno de los estados a comparar de acuerdo al estimulo en que esten
+                                string estadoEstimuloA = matrizSumaDirecta[filaA, h];
+                                string estadoEstimuloB = matrizSumaDirecta[filaB, h];
+                                //Ahora miramos si estan en la misma particion
+
+                                for (int c = 0; c < lista.Count; c++)
+                                {
+                                    if (lista.ElementAt(c).Contains(estadoEstimuloA))
+                                    {
+                                        if (lista.ElementAt(c).Contains(estadoEstimuloB))
+                                        {
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            //Aquí es donde va si no pertenecen a la misma particion
+                                            //Deberia romper dos for hacia arriba
+                                            seParticiono = true;
+                                            listaNuevaM.Add(estadoB);
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+
+                       
+                        }
+
+                    //Agregamos listaNueva a auxP1
+                    if(listaNuevaM.Count != 0)
+                    {
+                        auxP1.Add(listaNuevaM);
+                        //Aqui es donde se deberian eliminar
+                        for (int g = 0; g < auxP1.ElementAt(i).Count; g++)
+                        {
+                            if (auxP1.ElementAt(auxP1.Count - 1).Contains(auxP1.ElementAt(i).ElementAt(g)))
+                            {
+                                auxP1.ElementAt(i).RemoveAt(g);
+                                g--;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("Casi listo");
+                    
+                }
+
+                //Aqui miramos si no se han hecho nuevas particiones, si se han hecho, debe continuar, sino se acaba el programa
+                if (auxP1.Count == conjuntos)
+                {
+                    cierre = true;
+                }
+                else
+                {
+                    lista = auxP1;
+                }
+            }
+
+            comprobarMaquinaDeMoore(lista);
+
+            
+        }
+
+        public void comprobarMaquinaDeMoore(List<List<string>> listaFinal)
+        {
+            Boolean resultado = true;
+            for(int i = 0; i < listaFinal.Count; i++)
+            {
+                if(listaFinal.ElementAt(i).Count < 2)
+                {
+                    resultado = false;
+                    break;
+                }
+            }
+            resultado = resultado && (listaFinal.ElementAt(0).Contains(matrizM1[0, 0]) && listaFinal.ElementAt(0).Contains(matrizM2[0, 0]));
+
+            if (resultado)
+            {
+                for(int i = 0; i < listaFinal.Count && resultado; i++)
+                {
+                    Boolean encontroM1 = false;
+                    for(int j = 0; j < cantidadEstadosM1 && resultado; j++)
+                    {
+                        if (listaFinal.ElementAt(i).Contains(matrizM1[j, 0]))
+                        {
+                            //encontroM1 = true;
+                            Boolean encontroM2 = false;
+                            for (int k = 0; k < cantidadEstadosM2; k++)
+                            {
+                                if (listaFinal.ElementAt(i).Contains(matrizM2[k, 0]))
+                                {
+                                    encontroM1 = true;
+                                    encontroM2 = true;
+                                    break;
+                                }
+                            }
+                            if (!encontroM2)
+                            {
+                                resultado = false;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (!encontroM1)
+                    {
+                        resultado = false;
+                    }
+                }
+            }
+
+            Console.WriteLine(resultado);
         }
     }
 }
